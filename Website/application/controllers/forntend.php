ce9e8c4b5dd7b4f822eae18d8b899ce0aec54366
ASUS <layboe1111@gmail.com>
2018-02-12 15:27:22 +0700

    Front end Controller

--- Website/application/controllers/forntend.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class AdminController extends CI_Controller {
-	public function test(){
-		$this->load->view('forntend_website/templete/index.php');
-	}
-}
